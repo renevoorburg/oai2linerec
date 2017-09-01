9589e909c9992c44f46a598d3288ea3d4c9c672a
renevoorburg <voorburg@xs4all.nl>
2017-09-01 16:06:03 +0200

    retry retried...
    the 'retries now actually works' version with improved logging, cleaner code.

--- oai2linerec.sh
+++ oai2linerec.sh
@@ -3,21 +3,29 @@
 # A simple OAI-PMH harvester. Harvests records, aggregates them to one file, one line per record.
 # Requires perl, wget and xmllint (version 20708 or higher).
 # @author: René Voorburg / rene.voorburg@kb.nl
-# @version: 2017-08-31
+# @version: 2.0 dd 2017-09-01
 
-# 2017-08-15: Added gzip compression.
-# 2017-08-22: Refactored to use retry-function for robustness, no more temporary files.
-# 2017-08-25: Fixed incorrect testing for failed actions, added 'verbose' and 'debug' options.
-# 2017-08-28: New: harvest may now be paused and resumed. 
-# 2017-08-29: New: log slow actions when in debug mode.
-# 2017-08-31: Improved logging, better error handling.
+# 2017-09-01: The 'retries now actually works' version with improved logging, cleaner code.
 
+## declare global vars:
 
-# declare global vars:
-CMD=$0
+# defaults:
+LOGSLOW=10
+CURL='curl -fs'
+WGET='wget -q -t 3 -O -'
+COMPRESS=false
+VERBOSE=false
+DEBUG=false
+RESUMEPARAMS=''
+GZIP=cat
+
+# no need to change these:
+PROG=$0
 IDENTIFIERS_XP="//*[local-name()='header'][not(contains(@status, 'deleted'))]/*[local-name()='identifier']"
 RESUMPTION_XP="//*[local-name()='resumptionToken']/text()"
 METADATA_XP="//*[local-name()='metadata']"
+
+# init other globals:
 OUT=''
 FROM=''
 UNTIL=''
@@ -27,18 +35,15 @@ PREFIX=''
 URL=''
 IDENTIFIERS=''
 RESUMPTIONTOKEN=''
-GZIP=cat
-COMPRESS=false
-VERBOSE=false
-DEBUG=false
-RESUMEPARAMS=''
-LOGSLOW=10
+
+#required for single line XML normalization:
+export XMLLINT_INDENT='' 
 
 
 usage()
 {
     cat << EOF
-usage: $0 [OPTIONS] -o [outfile] -b [baseURL]
+usage: $PROG [OPTIONS] -o [outfile] -b [baseURL]
 
 This is a simple OAI-PMH harvester. It harvests records, compresses the <metadata> node and its content to a single line
 and appends it to the outfile.
@@ -60,12 +65,12 @@ OPTIONS:
    -r  token   Provide a resumptiontoken to continue a harvest
 
 EXAMPLE:
-$CMD -v -c -s sgd:register -p dcx -f 2012-02-03T09:04:21Z -o results.txt -b http://services.kb.nl/mdo/oai
+$PROG -v -c -s sgd:register -p dcx -f 2012-02-03T09:04:23Z -o results.txt -b http://services.kb.nl/mdo/oai
 
 EOF
 }
 
-progress()
+show_progress()
 {
     local out="$1"
 
@@ -74,23 +79,18 @@ progress()
     fi
 }
 
-fail()
+log()
 {
-    local msg="$1"
+    local msg="$@"
     local date=`date "+%Y-%m-%d %H:%M:%S"`
 
     echo "$date $msg" >&2
-    if [[ $msg == fatal* ]] || [[ $msg == Fatal* ]] ; then
-        exit 1
-    else
-        return 1
-    fi
 }
 
 retry() 
 {
-    local msg="$1"
-    local cmd="${@: 2}"
+    local cmd="$@"
+    local ret=0
     local n=1
     local max=3
     local delay=3
@@ -98,162 +98,211 @@ retry()
 
     while true; do
         $cmd && break || {
-        if [[ $n -lt $max ]]; then
-            ((n++))
-            sleep $delay;
-            if [ "$DEBUG" == "true" ] ; then
-                fail "Warning: retried action '$cmd'"
-            fi 
-        else
-            fail $msg
-        fi
+    		if [[ $n -lt $max ]]; then
+            	((n++))
+            	sleep $delay;
+            	if [ "$DEBUG" == "true" ] ; then
+                	log "Warning: '$cmd' failed, retrying."
+            	fi
+        	else
+            	log "Error: '$cmd' failed after retry $max."
+            	ret=1
+            	break
+        	fi
         }
     done
-    
+
     if [ "$DEBUG" == "true" ] ; then
     	local tend=`date "+%s"`
     	local tspend=$(($tend-$tstart))
     	if  [ "$tspend" -ge "$LOGSLOW" ] ; then
-    		fail "Warning: slow ($tspend s) action '$cmd'"
+    		log "Warning: '$cmd' slow ($tspend s)."
     	fi
     fi
     
+    return $ret
 }
 
 harvest_record()
 {
     local id="$1"
-    local metadata="`$GET "$BASE?verb=GetRecord$PREFIX&identifier=$id" | xmllint --xpath "//*[local-name()='metadata']" - 2>/dev/null || return 1`" 
-    local payload="`echo "$metadata" | xmllint --format - 2>/dev/null || return 1`"    
+    local metadata
+    local payload
+    
+    metadata="`$GET "$BASE?verb=GetRecord$PREFIX&identifier=$id" | xmllint --xpath "//*[local-name()='metadata']" - 2>/dev/null`"
+    if [ $? -ne 0 ] ; then return 1 ; fi
+     
+    payload="`echo "$metadata" | xmllint --format - 2>/dev/null`" 
+    if [ $? -ne 0 ] ; then return 1 ; fi
+       
     echo "$payload" | perl -pe 's@\n@@gi' | perl -pe 's@$@\n@' | $GZIP >> $OUT    
-    progress "."
+    show_progress "."
 }
 
 harvest_identifiers()
 {
     local url="$1"
-    local identifiers_xml="`$GET "$url" || return 1`"
-    local identifiers_selected="`echo "$identifiers_xml" | xmllint --xpath "$IDENTIFIERS_XP" - 2>/dev/null || return 1`"
+    local identifiers_xml
+    local identifiers_selected
+    
+    identifiers_xml="`$GET "$url"`"
+    if [ $? -ne 0 ] ; then return 1 ; fi
+    
+    identifiers_selected="`echo "$identifiers_xml" | xmllint --xpath "$IDENTIFIERS_XP" - 2>/dev/null`"
+    if [ $? -ne 0 ] ; then return 1 ; fi
  
     IDENTIFIERS="`echo "$identifiers_selected" | perl -pe 's@</identifier[^\S\n]*>@\n@g' | perl -pe 's@<identifier[^\S\n]*>@@'`" 
-    RESUMPTIONTOKEN="`echo "$identifiers_xml" | xmllint --xpath "$RESUMPTION_XP" - 2>/dev/null || return 1`"
+    RESUMPTIONTOKEN="`echo "$identifiers_xml" | xmllint --xpath "$RESUMPTION_XP" - 2>/dev/null`"
+    
     URL="$BASE?verb=ListIdentifiers&resumptionToken=$RESUMPTIONTOKEN"
 }
 
 
-# check for required environment:
-if ! hash perl 2>/dev/null; then
-    echo "Requires perl. Not found. Exiting."
-    exit
-fi
-if hash curl 2>/dev/null; then
-        GET='curl -s'
-elif hash wget  2>/dev/null; then
-        GET='wget -q -t 3 -O -'
-else
-    echo "Requires curl or wget. Not found. Exiting."
-    exit
-fi
-if ! hash xmllint 2>/dev/null; then
-    echo "Requires xmllint. Not found. Exiting."
-    exit
-fi
+get_parameters()
+{
+	local option 
+	
+	# check for required environment:
+	if ! hash perl 2>/dev/null; then
+		echo "Requires perl. Not found. Exiting."
+		exit 1
+	fi
+	if hash curl 2>/dev/null; then
+			GET="$CURL"
+	elif hash wget  2>/dev/null; then
+			GET="$WGET"
+	else
+		echo "Requires curl or wget. Not found. Exiting."
+		exit 1
+	fi
+	if ! hash xmllint 2>/dev/null; then
+		echo "Requires xmllint. Not found. Exiting."
+		exit 1
+	fi
+	
+	# read commandline opions
+	while getopts "hvdco:f:t:b:s:p:r:" option ; do
+		 case $option in
+			 h)
+				 usage
+				 exit 1
+				 ;;
+			 v)  VERBOSE=true
+				 RESUMEPARAMS="$RESUMEPARAMS -v"
+				 ;;
+			 d)  DEBUG=true
+				 RESUMEPARAMS="$RESUMEPARAMS -d"	
+				 ;;
+			 c)  COMPRESS=true
+				 RESUMEPARAMS="$RESUMEPARAMS -c"
+				 ;;
+			 o)
+				 OUT="$OPTARG"
+				 RESUMEPARAMS="$RESUMEPARAMS -o $OPTARG"
+				 ;;
+			 f)
+				 FROM="&from=$OPTARG"
+				 ;;
+			 t)
+				 UNTIL="&until=$OPTARG"
+				 ;;
+			 s)
+				 SET="&set=$OPTARG"
+				 ;;
+			 b)
+				 BASE="$OPTARG"
+				 RESUMEPARAMS="$RESUMEPARAMS -b $OPTARG"
+				 ;;
+			 p)
+				 PREFIX="&metadataPrefix=$OPTARG"
+				 RESUMEPARAMS="$RESUMEPARAMS -p $OPTARG"
+				 ;;
+			 r)
+				 RESUMPTIONTOKEN="$OPTARG"
+				 ;;
+			 ?)
+				 usage
+				 exit
+				 ;;
+		 esac
+	done
 
-# read commandline opions
-while getopts "hvdco:f:t:b:s:p:r:" OPTION ; do
-     case $OPTION in
-         h)
-             usage
-             exit 1
-             ;;
-         v)  VERBOSE=true
-             RESUMEPARAMS="$RESUMEPARAMS -v"
-             ;;
-         d)  DEBUG=true
-             RESUMEPARAMS="$RESUMEPARAMS -d"	
-             ;;
-         c)  COMPRESS=true
-             RESUMEPARAMS="$RESUMEPARAMS -c"
-             ;;
-         o)
-             OUT="$OPTARG"
-             RESUMEPARAMS="$RESUMEPARAMS -o $OPTARG"
-             ;;
-         f)
-             FROM="&from=$OPTARG"
-             ;;
-         t)
-             UNTIL="&until=$OPTARG"
-             ;;
-         s)
-             SET="&set=$OPTARG"
-             ;;
-         b)
-             BASE="$OPTARG"
-             RESUMEPARAMS="$RESUMEPARAMS -b $OPTARG"
-             ;;
-         p)
-             PREFIX="&metadataPrefix=$OPTARG"
-             RESUMEPARAMS="$RESUMEPARAMS -p $OPTARG"
-             ;;
-         r)
-             RESUMPTIONTOKEN="$OPTARG"
-             ;;
-         ?)
-             usage
-             exit
-             ;;
-     esac
-done
+	# set and test parameters:
+	if [ -z "$BASE" ] ; then
+		usage
+		exit 1
+	fi
+	if [ -z "$OUT" ] ; then
+		usage
+		exit 1
+	fi
 
-# set and test parameters:
-if [ -z "$BASE" ] ; then
-    usage
-    exit 1
-fi
-if [ -z "$OUT" ] ; then
-    usage
-    exit 1
-fi
-if [ "$COMPRESS" == "true" ] ; then
-    if ! hash gzip 2>/dev/null; then
-        echo "Compression requires gzip. Not found. Exiting."
-        exit 1
-    fi
-    GZIP=gzip
-    OUT=$OUT.gz
-fi
-export XMLLINT_INDENT='' #use this setting for single line XML normalization
+	if [ "$COMPRESS" == "true" ] ; then
+		if ! hash gzip 2>/dev/null; then
+			echo "Compression requires gzip. Not found. Exiting."
+			exit 1
+		fi
+		GZIP=gzip
+	 	OUT=$OUT.gz
+	fi
+	
+	#
+	if [ -z "$RESUMPTIONTOKEN" ] ; then
+		RESUMPTIONTOKEN='dummy'
+		URL="$BASE?verb=ListIdentifiers$FROM$UNTIL$PREFIX$SET"
+		>$OUT
+	else
+	   URL="$BASE?verb=ListIdentifiers&resumptionToken=$RESUMPTIONTOKEN"
+	fi
+	
+}
 
-#
-if [ -z "$RESUMPTIONTOKEN" ] ; then
-    RESUMPTIONTOKEN='dummy'
-    URL="$BASE?verb=ListIdentifiers$FROM$UNTIL$PREFIX$SET"
-    >$OUT
-else
-   progress "Continuing harvest with resumptiontoken $RESUMPTIONTOKEN\n"
-   URL="$BASE?verb=ListIdentifiers&resumptionToken=$RESUMPTIONTOKEN"
-fi
+exit_keypress()
+{
+	local listen="$1"
+	local msg="$2"
+	local alert="$3"
+	
+	echo -en "$msg"
+	read -t 2 -n 1 key && [[ $key = "$listen" ]] && echo -e "\n$alert" && exit 1
+	printf '\b%.0s' {1..100}
+	printf ' %.0s' {1..100}
+	printf '\b%.0s' {1..100}
+}
 
-# main loop:
-while [ -n "$RESUMPTIONTOKEN" ] ; do
+main()
+{
+	local id
 
-	# allow keypress 'p' to pause harvesting:
-    if [ -n "$IDENTIFIERS" ] ; then
-        echo -en "[ Press p to pauze harvest ]"
-        read -t 2 -n 1 key && [[ $key = p ]] && echo -e "\nHarvest paused.\nContinue harvest with $CMD -r '$RESUMPTIONTOKEN'$RESUMEPARAMS" && exit 1
-        echo -en "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-        echo -en "                            "
-        echo -en "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-    fi
+	while [ -n "$RESUMPTIONTOKEN" ] ; do
 
-	# harvest: 
-    retry "Fatal error obtaining identifiers from $URL" harvest_identifiers "$URL"
-    for i in `echo "$IDENTIFIERS" ` ; do
-        retry "Error harvesting record $i" harvest_record "$i"
-    done
-    progress "\n$RESUMPTIONTOKEN\n"
-done
+		# allow keypress 'p' to pause harvesting:
+		if [ -n "$IDENTIFIERS" ] ; then
+			exit_keypress "p" "[ Press p to pauze harvest ]" "\nHarvest paused.\nContinue harvest with $CMD -r '$RESUMPTIONTOKEN'$RESUMEPARAMS"
+		fi
 
-progress "done\n"
+		# harvest: 
+		retry harvest_identifiers "$URL"
+		if [ $? -ne 0 ] ; then exit 1 ; fi
+	
+		for id in `echo "$IDENTIFIERS" ` ; do
+			retry harvest_record "$id"
+		done
+		show_progress "\n$RESUMPTIONTOKEN\n"
+	done
+
+	show_progress "done\n"
+}
+
+
+get_parameters "$@"
+main
 exit 0
+
+
+
+
+
+
+
+
